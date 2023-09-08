0411323f545bbceb3b26ca64c5dce3970e772858
Rajit Deb <rajitdeb456@gmail.com>
2023-09-08 15:31:03 +0530

    First Commit

---
+++ app/src/main/java/com/rajit/learnrxjava/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.rajit.learnrxjava
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rajit.learnrxjava.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
